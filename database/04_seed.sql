ea37c98e4219fd2a1a5fe11c929422f5d21fa459
vlodzina-ship-it <vlodzina@gmail.com>
2026-03-26 09:20:09 +0100

    Create 04_seed.sql

---
+++ database/04_seed.sql
@@ -0,0 +1,9 @@
+-- ==========================================
+-- 04_seed.sql
+-- Docházkový systém - výchozí data
+-- ==========================================
+
+-- Obsah:
+-- - app_settings
+-- - základní offices
+-- - případně první admin setup
