df5e26f8034a7a3e24f8fdbf5defebcaa418a7ae
vlodzina-ship-it <vlodzina@gmail.com>
2026-03-26 09:18:33 +0100

    Create 02_functions.sql

---
+++ database/02_functions.sql
@@ -0,0 +1,9 @@
+-- ==========================================
+-- 02_functions.sql
+-- Docházkový systém - funkce a RPC
+-- ==========================================
+
+-- Obsah:
+-- - helper funkce
+-- - RPC funkce
+-- - trigger funkce
