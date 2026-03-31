1c9ead9705a34063c3e5b6ff4d888797b576edce
vlodzina-ship-it <vlodzina@gmail.com>
2026-03-31 15:02:14 +0200

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,1 +1,15 @@
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
 
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(key => caches.delete(key)));
+    await self.clients.claim();
+  })());
+});
+
+self.addEventListener('fetch', () => {
+  // úmyslně bez cache
+});
