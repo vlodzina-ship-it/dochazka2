f4f0d49aa40245e955bf9844b02538317c3b3e7f
vlodzina-ship-it <vlodzina@gmail.com>
2026-03-31 14:24:33 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://wgnzqdleoiyuukgololm.supabase.co",
+  SUPABASE_KEY: "sb_publishable_qa7SC3zS7C46oDudqrAQ4g_b-e0wrCQ",
+  APP_VERSION: "2026-03-31-product-1-0"
+};
